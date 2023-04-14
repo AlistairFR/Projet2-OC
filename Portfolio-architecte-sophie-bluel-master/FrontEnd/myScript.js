b14ccb6f15755a91f95ff16512333a08ea72e27b
AlistairFR <jbwinckel62370@gmail.com>
2023-04-14 11:30:19 +0200

    Roadmap commentaires JS pour le mode édition

--- Portfolio-architecte-sophie-bluel-master/FrontEnd/myScript.js
+++ Portfolio-architecte-sophie-bluel-master/FrontEnd/myScript.js
@@ -100,65 +100,75 @@ function filterWorks(e) {
 
 // Système de login
 
-// Formulaire de login
-const form = {
-    email: document.getElementById("email"),
-    password: document.getElementById("password"),
-    submitButton: document.getElementById("submit")
-};
+    // Formulaire de login
+    const form = {
+        email: document.getElementById("email"),
+        password: document.getElementById("password"),
+        submitButton: document.getElementById("submit")
+    };
 
-// Message d'erreur pour login incorrect
-const errorDiv = document.createElement("div");
-errorDiv.textContent = "Adresse mail ou mot de passe incorrect";
-document.getElementById("loginForm").appendChild(errorDiv);
-errorDiv.style.background = "pink";
-errorDiv.style.color = "red";
-errorDiv.style.padding = "15px 0px";
-errorDiv.style.margin= "25px";
-errorDiv.classList.add("hidden");
+    // Message d'erreur pour login incorrect
+    const errorDiv = document.createElement("div");
+    errorDiv.textContent = "Adresse mail ou mot de passe incorrect";
+    document.getElementById("loginForm").appendChild(errorDiv);
+    errorDiv.style.background = "pink";
+    errorDiv.style.color = "red";
+    errorDiv.style.padding = "15px 0px";
+    errorDiv.style.margin= "25px";
+    errorDiv.classList.add("hidden");
 
-// Fonctions pour gérer le token dans le local Storage
-const saveElement = (key, value) => {
-    localStorage.setItem(key, value)
-};
+    // Fonctions pour gérer le token dans le local Storage
+    const saveElement = (key, value) => {
+        localStorage.setItem(key, value)
+    };
 
-const getElement = (key) => {
-    return localStorage.getItem(key)
-};
+    const getElement = (key) => {
+        return localStorage.getItem(key)
+    };
 
-const removeElement = (key) => {
-    localStorage.removeItem(key)
-};
+    const removeElement = (key) => {
+        localStorage.removeItem(key)
+    };
 
-// Quand on clique sur le bouton "Se connecter"
-if (form.submitButton){
-    let submit = form.submitButton.addEventListener("click", (e) => {
-        e.preventDefault();
+    // Quand on clique sur le bouton "Se connecter"
+    if (form.submitButton){
+        let submit = form.submitButton.addEventListener("click", (e) => {
+            e.preventDefault();
 
-        fetch(`${apiUrl}/users/login`, {
-            method: "POST",
-            headers: {
-                "Accept": "application/json",
-                "Content-Type": "application/json",
-                // A mettre dans les posts avec auth : "Authorization": `Bearer ${localStorage.getItem("token")}`
-            },
-            body: JSON.stringify({
-                email: form.email.value,
-                password: form.password.value,
-            }),
-            })
-            .then((response) => response.json())
-            .then((response) => {
-                saveElement("authToken", response.token)
-                if (getElement("authToken") !== "undefined"){
-                    window.location.href = "index.html"
-                }
-                    else {
-                        errorDiv.removeAttribute("class");
+            fetch(`${apiUrl}/users/login`, {
+                method: "POST",
+                headers: {
+                    "Accept": "application/json",
+                    "Content-Type": "application/json",
+                    // A mettre dans les posts avec auth : "Authorization": `Bearer ${localStorage.getItem("token")}`
+                },
+                body: JSON.stringify({
+                    email: form.email.value,
+                    password: form.password.value,
+                }),
+                })
+                .then((response) => response.json())
+                .then((response) => {
+                    saveElement("authToken", response.token)
+                    if (getElement("authToken") !== "undefined"){
+                        window.location.href = "index.html"
                     }
-            })
-            .catch( (error) => {
-                console.error(error)
-            })
-    });
-}
+                        else {
+                            errorDiv.removeAttribute("class");
+                        }
+                })
+                .catch( (error) => {
+                    console.error(error)
+                })
+        });
+    }
+
+// Page index quand authentifié
+
+    // Mode édition de la page index
+        //Bandereau mode édiiton
+        //icônes "modifier"
+
+    // Création de la modale
+
+    // Ouvrir la modale quand authentifié ET clic sur icône
